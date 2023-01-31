e38c499c201ecdc15cec65f82568bb71808d7b20
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-30 21:19:51 -0500

    create routes directory and users file with express import and first route with test comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js'
 
 const app = express();
 const PORT = 3001;
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+import express from express;
+
+const router = express.Router();
+
+router.get('/users', () => {
+  resizeBy.send('Hello World!')
+});
+
+export default router;
