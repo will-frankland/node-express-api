cd658e21e50a3a1100078b072329db4b05cb4f4f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-30 22:14:46 -0500

    refactor code and minimize use of constants

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
@@ -20,10 +21,12 @@ router.get('/', (req, res) => {
   res.send(users)
 });
 
+
+// Used to create users
 router.post('/', (req, res) => {
   const user = (req.body);
 
-  users.push(user);
+  users.push({...user, id: uudiv4() });
 
   res.send(`User with the username ${user.firstName} added to the database`);
 });
