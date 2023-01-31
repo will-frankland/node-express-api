fcc3b0515a0ea0037752c34d66085a6b738885e3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-30 21:36:05 -0500

    add user data and test with get request

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,27 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstnName: "Joel",
+    lastName: "Zimmerman",
+    age: 42
+  },
+  {
+    firstnName: "Lewis",
+    lastName: "Hamilton",
+    age: 38
+  },
+];
 
 // All routes in here are already starting with /users
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log(users);
+  res.send(users)
+});
+
+router.post('/', (req, res) => {
+  
 });
 
 export default router;
